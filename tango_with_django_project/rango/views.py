4a10c9800d04b2f76496e0aeacd0599a206e86e9
Django python <djangopython@MacBook-Pro-Pri.local>
2018-07-28 12:16:22 +0700

    tantangan about

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/><strong><a href='/rango/about/'>About</a></strong>")
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/><strong><a href='/rango/'>Index</a></strong>")
